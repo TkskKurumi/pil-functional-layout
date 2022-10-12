003b98933b65141f77988a9d52fc4538215371b3
TkskKurumi <zafkielkurumi@gmail.com>
2022-10-12 10:00:34 +0800

    Keyword default value

--- pil_functional_layout/__init__.py
+++ pil_functional_layout/__init__.py
@@ -1,6 +1,9 @@
 from .widgets import *
-def Keyword(kwa):
+def Keyword(kwa, *args):
     def f(**kwargs):
-        nonlocal kwa
-        return kwargs[kwa]
+        nonlocal args
+        if(args):
+            return kwargs.get(kwa, args[0])
+        else:
+            return kwargs[kwa]
     return f
